7d7eced901d8ed4a3838cab81023e8565bb36c71
Janus Huang <januswow@gmail.com>
2016-10-06 11:13:10 +0800

    LayersToPNG JH v1.1.0
    v1.1.0
    1. Prevent getting error from incorrect suffixes.
    2. Export images through Save For Web.
    3. Calculate data by layer bounds.
    4. Allow corp pixles out of canvas or not.
    5. Fixed incorrect position after layer rotated.

--- Photoshop-LayersToPNG_JH.jsx
+++ Photoshop-LayersToPNG_JH.jsx
@@ -1,16 +1,26 @@
+// Spine Layers to png
 // This script exports photoshop layers as individual PNGs. It also
 // writes a JSON file that can be imported into Spine where the images
 // will be displayed in the same positions and draw order.
 
-// Mod by JanusHuang (e-mail:januswow@gmail.com)
+// Mod by JanusHuang
+// Version 1.1.0
+// (e-mail:januswow@gmail.com)
 //
 // Gives you the ability to easily do more optimization to texture resolution.
 //
 // You are able to set export scale and rotation values for a specific layer in Photoshop and maintain
 // the size and rotation of attachments after importing to Spine, just the same as you see in Photoshop.
 //
+// v1.0.0
 // 1. When exporting images, scale and rotation values will automatically apply according to the parameters specified in the Photoshop layer name.
 // 2. When exporting JSON, scale and rotation values specified in the Photoshop layer name will be written as JSON data.
+// v1.1.0
+// 1. Prevent getting error from incorrect suffixes.
+// 2. Export images through Save For Web.
+// 3. Calculate data by layer bounds.
+// 4. Allow corp pixles out of canvas or not.
+// 5. Fixed incorrect position after layer rotated.
 //
 // You can define scale and rotation by adding suffixes to layer name.
 // Define scale by adding suffix ' #s' to layer name.
@@ -28,13 +38,19 @@ var ignoreHiddenLayers = true;
 var pngScale = 1;
 var groupsAsSkins = false;
 var useRulerOrigin = false;
+var corpToCanvas = false;
 var imagesDir = "./images/";
 var projectDir = "";
 var padding = 1;
 
-// JH MOD - variables for custom rotate and scale from ps layer name
-var patternRotate = "#r";
-var patternScale = "#s";
+// JH Version Variables
+var JHVersion = "1.1.0"
+var patternRotate = " #r";
+var patternScale = " #s";
+// minimum width/height pixels of output image
+var customScaleMin = 4;
+var docWidth;
+var docHeight;
 
 // IDs for saving settings.
 const settingsID = stringIDToTypeID("settings");
@@ -44,6 +60,7 @@ const writeJsonID = stringIDToTypeID("writeJson");
 const ignoreHiddenLayersID = stringIDToTypeID("ignoreHiddenLayers");
 const groupsAsSkinsID = stringIDToTypeID("groupsAsSkins");
 const useRulerOriginID = stringIDToTypeID("useRulerOrigin");
+const corpToCanvasID = stringIDToTypeID("corpToCanvas");
 const pngScaleID = stringIDToTypeID("pngScale");
 const imagesDirID = stringIDToTypeID("imagesDir");
 const projectDirID = stringIDToTypeID("projectDir");
@@ -80,6 +97,14 @@ function run () {
 
 	activeDocument.duplicate();
 
+    // Save for web options
+ 	var outputExtension = ".png";
+    var sfwOptions;
+    sfwOptions = new ExportOptionsSaveForWeb();
+    sfwOptions.format = SaveDocumentType.PNG;
+    // save to PNG24
+    sfwOptions.PNG8 = false;
+
 	// Output template image.
 	if (writeTemplate) {
 		if (pngScale != 1) {
@@ -87,10 +112,10 @@ function run () {
 			storeHistory();
 		}
 
-		var file = new File(absImagesDir + "template");
+		var file = new File(absImagesDir + "template" + outputExtension);
 		if (file.exists) file.remove();
 
-		activeDocument.saveAs(file, new PNGSaveOptions(), true, Extension.LOWERCASE);
+		activeDocument.exportDocument(file, ExportType.SAVEFORWEB, sfwOptions);
 
 		if (pngScale != 1) restoreHistory();
 	}
@@ -135,12 +160,21 @@ function run () {
 	// Output slots.
 	var slotsCount = countAssocArray(slots);
 	var slotIndex = 0;
+
+	// Output template slot
+	if (writeTemplate) {
+		json += '\t{"name":"' + "template" + '","bone":"root","attachment":"' + "template" + '"}';
+		json += slotsCount > 0 ? ",\n" : "\n";
+	}
+
 	for (var slotName in slots) {
+
 		if (!slots.hasOwnProperty(slotName)) continue;
 
 		// Use image prefix if slot's attachment is in the default skin.
 		var attachmentName = slotName;
 		var defaultSkinLayers = skins["default"];
+		
 		for (var i = defaultSkinLayers.length - 1; i >= 0; i--) {
 			if (layerName(defaultSkinLayers[i]) == slotName) {
 				attachmentName = slotName;
@@ -157,6 +191,9 @@ function run () {
 	// Output skins.
 	var skinsCount = countAssocArray(skins);
 	var skinIndex = 0;
+	docWidth = activeDocument.width.as("px");
+	docHeight = activeDocument.height.as("px");
+
 	for (var skinName in skins) {
 		if (!skins.hasOwnProperty(skinName)) continue;
 		json += '\t"' + skinName + '":{\n';
@@ -164,6 +201,19 @@ function run () {
 		var skinLayers = skins[skinName];
 		var skinLayersCount = skinLayers.length;
 		var skinLayerIndex = 0;
+
+		// Output Template
+		if (writeTemplate) {
+			json += '\t\t"' + "template" + '":{"' + "template" + '":{'
+			if (useRulerOrigin) {
+				json += '"x":' + docWidth / 2 + ',"y":' + docHeight / 2 * -1;
+			} else {
+				json += '"x":' + docWidth / 2 + ',"y":' + docHeight / 2;
+			}			
+			json += ',"width":' + docWidth + ',"height":' + docHeight + '}}';
+			json += skinLayersCount > 0 ? ",\n" : "\n";
+		}
+
 		for (var i = skinLayersCount - 1; i >= 0; i--) {
 			var layer = skinLayers[i];
 
@@ -178,76 +228,84 @@ function run () {
 				attachmentName = skinName + "/" + slotName;
 			}
 
-			// JH MOD - variables for rotation and scale from ps layer name
+			layer.visible = true;
+
+			// get data from bounds
+			var boundsCenter = getBoundsCenter(layer);
+			var x = boundsCenter[0];
+			var y = boundsCenter[1];
 			var customRotate = 0;
 			var customScale = 1;
 
-			if (sourceLayerName.lastIndexOf(" #") != -1) {
-				// had custom rotate and scale value in layer name
-				customRotate = parseFloat(GetRotate(sourceLayerName));
-				customScale = parseFloat(GetScale(sourceLayerName));
+			// get custom scale and rotation from layer name
+			if (hasCustomParameters(sourceLayerName)) {
+				customRotate = GetRotate(sourceLayerName);
+				customScale = GetScale(sourceLayerName);
+				// prevent resolution of output image too small
+				if (customScale != 1) customScale = checkMinimumCustomScale(layer, customScale);
+			}
+			
+			// Rotate image
+			if (customRotate != 0) {
+				layer.rotate(customRotate, AnchorPosition.MIDDLECENTER);
+				var rotatedCenter = getBoundsCenter(layer);
+				var fixedCenter = fixRotatedOffset(rotatedCenter, boundsCenter, customRotate);
+				x = fixedCenter[0];
+				y = fixedCenter[1];
+			}
+			
+			// Corp to canvas and trim layer
+			if (!corpToCanvas || customRotate != 0) extendCanvas(layer);
+			if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, false, false, true, true);
+			if (corpToCanvas && customRotate == 0) {
+				x = getCorpBoundsCenter(layer)[0];
+				y = getCorpBoundsCenter(layer)[1];
+			}
+			if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, true, true, false, false);
+
+			// Scale image
+			if (pngScale != 1 || customScale != 1) {
+				scaleImage(customScale);
 			}
 
-			var x = activeDocument.width.as("px") * pngScale;
-			var y = activeDocument.height.as("px") * pngScale;
-
-			layer.visible = true;
-			if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, false, true, true, false);
-			x -= activeDocument.width.as("px") * pngScale;
-			y -= activeDocument.height.as("px") * pngScale;
-			if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, true, false, false, true);
-			var width = activeDocument.width.as("px") * pngScale + padding * 2;
-			var height = activeDocument.height.as("px") * pngScale + padding * 2;
-
-			// Save image.
-			if (writePngs) {
-
-				// JH MOD - scale image
-				if (pngScale != 1 || customScale != 1) {
-					scaleImage(customScale);
-				}
-
-				if (customRotate != 0) {
-					rotateImage(customRotate); // JH MOD - rotate image
-					if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, true, true, true, true);
-				}
-
+			// Padding
+			if (padding > 0) {
 				var canvasWidth = activeDocument.width.as("px") + padding * 2;
 				var canvasHeight = activeDocument.height.as("px") + padding * 2;
-				if (padding > 0) activeDocument.resizeCanvas(canvasWidth, canvasHeight, AnchorPosition.MIDDLECENTER);				
+				activeDocument.resizeCanvas(canvasWidth, canvasHeight, AnchorPosition.MIDDLECENTER);
+			}
 
+			// Save image
+			if (writePngs) {
 				if (skinName != "default") new Folder(absImagesDir + skinName).create();
-				activeDocument.saveAs(new File(absImagesDir + attachmentName), new PNGSaveOptions(), true, Extension.LOWERCASE);
+				activeDocument.exportDocument(new File(absImagesDir + attachmentName + outputExtension), ExportType.SAVEFORWEB, sfwOptions);
 			}
 
 			restoreHistory();
 			layer.visible = false;
 
-			x += Math.round(width) / 2;
-			y += Math.round(height) / 2;
-
-			// Make relative to the Photoshop document ruler origin.
 			if (useRulerOrigin) {
-				x -= xOffSet * pngScale;
-				y -= activeDocument.height.as("px") * pngScale - yOffSet * pngScale; // Invert y.
+				y = toRulerCoord([x,y])[1];
+			} else {
+				y = toSpineCoord([x,y])[1];
 			}
-			
-			// JH MOD : Write attachments' information to JSON
-			// write head
+
+			var outScale = mathRound(1 / customScale, 2);
+
+			// Write JSON
 			if (attachmentName == placeholderName) {
 				json += '\t\t"' + slotName + '":{"' + placeholderName + '":{'
 			} else {
 				json += '\t\t"' + slotName + '":{"' + placeholderName + '":{"name":"' + attachmentName + '", '
 			}
 			// write position
-			json += '"x":' + x + ',"y":' + y;
+			json += '"x":' + mathRound(x, 1) + ',"y":' + mathRound(y, 1);
 			// write custom scale
-			if (customScale != 1) json += ',"scaleX":' + 1 / customScale + ',"scaleY":' + 1 / customScale;
+			if (customScale != 1) json += ',"scaleX":' + outScale + ',"scaleY":' + outScale;
 			// write custom rotate
 			if (customRotate != 0) json += ',"rotation":' + customRotate;
 			// write size
-			json += ',"width":' + Math.round(width) + ',"height":' + Math.round(height);
-			// write end
+			json += ',"width":' + Math.round(canvasWidth) + ',"height":' + Math.round(canvasHeight);
 			json += '}}';
 
 			skinLayerIndex++;
@@ -273,6 +331,8 @@ function run () {
 		file.write(json);
 		file.close();
 	}
+
+	alert("Done!\n" + skinLayersCount + " layers exported.")
 }
 
 // Dialog and settings:
@@ -287,7 +347,7 @@ function showDialog () {
 		return;
 	}
 
-	var dialog = new Window("dialog", "Spine LayersToPNG (Mod by JanusHuang)");
+	var dialog = new Window("dialog", "Spine LayersToPNG JH v" + JHVersion + " by JanusHuang");
 	dialog.alignChildren = "fill";
 
 	var checkboxGroup = dialog.add("group");
@@ -300,6 +360,9 @@ function showDialog () {
 			writeTemplateCheckbox.value = writeTemplate;
 			var writeJsonCheckbox = group.add("checkbox", undefined, " Write Spine JSON");
 			writeJsonCheckbox.value = writeJson;
+			var emptyCheckbox = group.add("checkbox", undefined, " Empty");
+			emptyCheckbox.visible = false;
+			emptyCheckbox.enabled = false;
 		group = checkboxGroup.add("group");
 			group.orientation = "column";
 			group.alignChildren = "left";
@@ -309,6 +372,8 @@ function showDialog () {
 			groupsAsSkinsCheckbox.value = groupsAsSkins;
 			var useRulerOriginCheckbox = group.add("checkbox", undefined, " Use ruler origin as 0,0");
 			useRulerOriginCheckbox.value = useRulerOrigin;
+			var corpToCanvasCheckbox = group.add("checkbox", undefined, " Corp to canvas");
+			corpToCanvasCheckbox.value = corpToCanvas;
 
 	var slidersGroup = dialog.add("group");
 		group = slidersGroup.add("group");
@@ -377,6 +442,7 @@ function showDialog () {
 		if (scaleValue > 0 && scaleValue <= 100) pngScale = scaleValue / 100;
 		groupsAsSkins = groupsAsSkinsCheckbox.value;
 		useRulerOrigin = useRulerOriginCheckbox.value;
+		corpToCanvas = corpToCanvasCheckbox.value;
 		imagesDir = imagesDirText.text;
 		projectDir = projectDirText.text;
 		var paddingValue = parseInt(paddingText.text);
@@ -432,6 +498,7 @@ function loadSettings () {
 	if (settings.hasKey(pngScaleID)) pngScale = settings.getDouble(pngScaleID);
 	if (settings.hasKey(groupsAsSkinsID)) groupsAsSkins = settings.getBoolean(groupsAsSkinsID);
 	if (settings.hasKey(useRulerOriginID)) useRulerOrigin = settings.getBoolean(useRulerOriginID);
+	if (settings.hasKey(corpToCanvasID)) corpToCanvas = settings.getBoolean(corpToCanvasID);
 	if (settings.hasKey(imagesDirID)) imagesDir = settings.getString(imagesDirID);
 	if (settings.hasKey(projectDirID)) projectDir = settings.getString(projectDirID);
 	if (settings.hasKey(paddingID)) padding = settings.getDouble(paddingID);
@@ -446,6 +513,7 @@ function saveSettings () {
 	settings.putDouble(pngScaleID, pngScale);
 	settings.putBoolean(groupsAsSkinsID, groupsAsSkins);
 	settings.putBoolean(useRulerOriginID, useRulerOrigin);
+	settings.putBoolean(corpToCanvasID, corpToCanvas);
 	settings.putString(imagesDirID, imagesDir);
 	settings.putString(projectDirID, projectDir);
 	settings.putDouble(paddingID, padding);
@@ -460,10 +528,45 @@ function scaleImage (customScale) {
 	activeDocument.resizeImage(UnitValue(imageSize * pngScale * customScale, "px"), null, null, ResampleMethod.BICUBICSHARPER);
 }
 
-// JH MOD
 function rotateImage (customRotate) {
 	if (customRotate == undefined) customRotate = 0;
 	activeDocument.rotateCanvas(customRotate);
+}
+
+function fixRotatedOffset(rotatedCenter, originalCenter, degrees){
+	// Get right position of rotated layer to write JSON data
+	var rcx = rotatedCenter[0];
+	var rcy = rotatedCenter[1];
+	var ocx = originalCenter[0];
+	var ocy = originalCenter[1];
+	// angle must be counter-clockwise
+	var theta = toRadians(degrees * -1);
+
+	// translate point to origin
+	var tempX = ocx - rcx;
+	var tempY = ocy - rcy;
+
+	// now apply rotation
+	var rotatedX = parseFloat(tempX * Math.cos(theta) - tempY * Math.sin(theta));
+	var rotatedY = parseFloat(tempX * Math.sin(theta) + tempY * Math.cos(theta));
+
+	// position to JSON
+	var outX = ocx - rotatedX;
+	var outY = ocy - rotatedY;
+
+	return [outX, outY];
+}
+
+function checkMinimumCustomScale (layer, customScale) {
+	// Prevent resolution of output image too small
+	var boundsSize = getBoundsSize(layer);
+	var result = customScale;
+	var minPixelsOrig = Math.min(boundsSize[0], boundsSize[1]);
+	var minPixels = minPixelsOrig * pngScale * customScale;
+
+	if (minPixels < customScaleMin) result = Math.ceil(mathRound(customScaleMin / minPixelsOrig, 3));
+
+	return result;
 }
 
 var historyIndex;
@@ -505,6 +608,17 @@ function absolutePath (path) {
 	return path;
 }
 
+// Mathematics utility:
+
+function toRadians (degrees) {
+	return degrees * Math.PI / 180;
+}
+
+function mathRound(num, decimal) {
+	var f = decimal > 0 ? Math.pow(10, decimal) : 1;
+	return Math.round(num * f) / f;
+}
+
 // JavaScript utility:
 
 function countAssocArray (obj) {
@@ -527,58 +641,157 @@ function stripSuffix (str, suffix) {
 	return str;
 }
 
-// JH MOD - remove custom rotation and scale string from photoshop layer name
 function layerName (layer) {
-	result = stripSuffix(trim(layer.name), ".png").replace(/[:\/\\*\?\"\<\>\|]/g, "");
-	endIndex = result.indexOf(" #");
+	// Get layer name without custom scale and rotation parameters string
+	var result = stripSuffix(trim(layer.name), ".png").replace(/[:\/\\*\?\"\<\>\|]/g, "");
+	var endIndex = result.indexOf(" #");
 	if (endIndex != -1) {
 		result = result.substring(0, endIndex);
 	}
+
 	return result
 }
 
-// JH MOD - this is original version of layerName function
 function srcLayerName (layer) {
-	result = stripSuffix(trim(layer.name), ".png").replace(/[:\/\\*\?\"\<\>\|]/g, "");
-	return result
+	// Original version of layerName function
+	return stripSuffix(trim(layer.name), ".png").replace(/[:\/\\*\?\"\<\>\|]/g, "");
 }
 
-// JH MOD START - functions for getting custom rotation and scale information from ps layer name
-function GetPropertyInfo (layerName, pattern) {
-	patternIndex = layerName.lastIndexOf(pattern);
-	if (patternIndex != -1) {
-		patternEndIndex = patternIndex + pattern.length;
-		propertyString = layerName.substring( patternEndIndex, layerName.length );
+// Custom properties utility :
 
-		endIndex = propertyString.indexOf(" ");
+function hasCustomParameters (layerName) {
+	var result = false;
+	if (layerName.search(patternRotate) != -1 || layerName.search(patternScale) != -1) {
+		result = true;
+	}
+
+	return result;
+}
+
+function GetPropertyInfo (layerName, pattern) {
+	var result = NaN;
+	var patternIndex = layerName.lastIndexOf(pattern);
+	var propertyInfo;
+
+	if (patternIndex != -1) {
+		var patternEndIndex = patternIndex + pattern.length;
+		var propertyString = layerName.substring( patternEndIndex, layerName.length );
+		var endIndex = propertyString.indexOf(" ");
 		if (endIndex != -1) {
 			propertyInfo = propertyString.substring(0, endIndex);
 		} else {
 			propertyInfo = propertyString;
 		}
-		return propertyInfo
-	} else {
-		return undefined;
+		result = parseFloat(propertyInfo);
 	}
+
+	return result;
 }
+
 function GetRotate (layerName) {
-	value = GetPropertyInfo(layerName, patternRotate);
-	if (value == undefined)
-	{
-		result = 0;
-	} else {
-		result = value;
-	}
+	var result = 0;
+	var value = GetPropertyInfo(layerName, patternRotate);
+	if (!isNaN(value)) result = value;
+
 	return result;
 }
+
 function GetScale (layerName) {
-	value = GetPropertyInfo(layerName, patternScale);
-	if (value == undefined)
-	{
-		result = 1;
-	} else {
-		result = value;
-	}
+	var result = 1;
+	var value = GetPropertyInfo(layerName, patternScale);
+	if (!isNaN(value)) result = value;
+
 	return result;
 }
-// JH MOD END - functions for getting custom rotation and scale information from ps layer name
+
+// Bounds and Corp to canvas utility :
+
+function clampXToCanvas(num, axis) {
+	return Math.min( Math.max(num, 0), docWidth );
+}
+
+function clampYToCanvas(num, axis) {
+	return Math.min( Math.max(num, 0), docHeight );
+}
+
+function clampBoundsToCanvas(bounds) {
+	var x1 = clampXToCanvas(bounds[0]);
+	var y1 = clampYToCanvas(bounds[1]);
+	var x2 = clampXToCanvas(bounds[2]);
+	var y2 = clampYToCanvas(bounds[3]);
+
+	return [x1, y1, x2, y2];
+}
+
+function getBoundsValue(layer) {
+	return [layer.bounds[0].value, layer.bounds[1].value, layer.bounds[2].value, layer.bounds[3].value];
+}
+
+function getBoundsSize(layer) {
+	var bounds = getBoundsValue(layer);
+	var width = bounds[2] - bounds[0];
+	var height = bounds[3] - bounds[1];
+
+	return [width, height];
+}
+
+function getBoundsCenter(layer) {
+	var bounds = getBoundsValue(layer);
+	var size = getBoundsSize(layer);
+	var x = ( bounds[0] + size[0] / 2 ) * pngScale;
+	var y = ( bounds[1] + size[1] / 2 ) * pngScale;
+
+	return [x, y];
+}
+
+function getCorpBoundsCenter(layer) {
+	var clampBounds = clampBoundsToCanvas(getBoundsValue(layer));
+	var x1 = clampBounds[0];
+	var y1 = clampBounds[1];
+	var x2 = clampBounds[2];
+	var y2 = clampBounds[3];
+	var width = x2 - x1;
+	var height = y2 - y1;
+	var x = ( x1 + width / 2 ) * pngScale;
+	var y = ( y1 + height / 2 ) * pngScale;
+
+	return [x, y];
+}
+
+function getOutOfCanvasDist(layer) {
+	var bounds = getBoundsValue(layer);
+	var x1 = bounds[0];
+	var y1 = bounds[1];
+	var x2 = activeDocument.width.as("px") - bounds[2];
+	var y2 = activeDocument.height.as("px") - bounds[3];
+	var dist = Math.min(x1, y1, x2, y2);
+
+	return dist;
+}
+
+function isOutOfCanvas(layer) {
+	if (getOutOfCanvasDist(layer) < 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function extendCanvas(layer) {
+	// Extend Canvas size for not corpping visible pixels
+	if (!isOutOfCanvas(layer)) return;
+	var dist = Math.abs(getOutOfCanvasDist(layer)) * 2;
+	var w = activeDocument.width.as("px") + dist;
+	var h = activeDocument.height.as("px") + dist;
+	activeDocument.resizeCanvas(w, h, AnchorPosition.MIDDLECENTER);
+}
+
+// Coordinates utility :
+
+function toSpineCoord(pos) {
+	return [pos[0], docHeight - pos[1]];
+}
+
+function toRulerCoord(pos) {
+	return [pos[0], pos[1] * -1];
+}
